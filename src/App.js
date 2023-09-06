444844c5c5c286b48809343972e97da28c51fc74
goremi <ffdan@naver.com>
2023-09-06 15:47:39 +0900

    build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
 
-function App() {
+export default function App() {
+   const [data,setData]=useState({members:[]})
+     useEffect(()=>{
+        fetch('/api').then(res =>res.json()).then(items=>setData(items))
+
+    },[])
+    console.log(data)
+    console.log(data.members)
+    const submitHandler=()=>{
+      fetch('/data',{
+       method:'POST',
+       body:JSON.stringify({
+        user:'goremi',
+       }),
+       headers:{
+        'Content-Type': 'application/json' 
+       }
+      }).then(res=>res.json()).then(data=>console.log(data))
+    }
+     
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>  
+    <div onClick={submitHandler} style={{display:'flex', justifyContent:'center',alignItems:'center',height:'100vh',}}>
+     {data && data.members.length > 0 &&
+      data.members.map((item,index)=> <p style={{ paddingRight:'20px',position:'relative'}} key={index}>{item}</p>)
+    }
+       <button style={{position:'absolute',top:'400px'}}>클릭</button>
+</div>
 
-export default App;
+
+
+    </>
+  )
+}
